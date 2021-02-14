cc900e47326a2e713489a1484739715937df1775
Dgeison <paradg@gmail.com>
2021-02-14 00:05:48 -0300

    desestruturação de dados

--- instructors.js
+++ instructors.js
@@ -1,36 +1,43 @@
 const fs = require("fs");
 const data = require("./data.json");
 
-
 // CREATE
 exports.post = function (req, res) {
-  // if (req.body.name != "") {
-  //   return res.send("Prencha o nome do instrutor");
-  // }
-
-  //["avatar_URL","name","birth","gender","services"]
-  //{"avatar_URL":"http://google.com","name":"Dgeison Peixoto","birth":"2021-02-13","gender":"M","services":""}
   const keys = Object.keys(req.body);
 
   for (key of keys) {
     if (req.body[key] == "") return res.send("Please, fill all fields!");
   }
 
-  req.body.birth = Date.parse(req.body.birth)
+  req.body.birth = Date.parse(req.body.birth);
+  req.body.created_at = Date.now();
+  req.body.id = Number(data.instructors.length + 1);
 
-  req.body.created_at = Date.now()
-  req.body.id = data.instructors.length + 1
+  const {
+    avatar_url,
+    birth,
+    created_at,
+    id,
+    name,
+    gender,
+    services,
+  } = req.body;
 
-  // []
-  data.instructors.push(req.body); // [{...}, {...}]
+  data.instructors.push(
+    avatar_url,
+    birth,
+    created_at,
+    id,
+    name,
+    gender,
+    services
+  );
 
   fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
     if (err) return res.send("Write file errors!");
 
     return res.redirect("/instructors");
   });
-
-  //   return res.send(req.body);
 };
 
 // UPDATE
